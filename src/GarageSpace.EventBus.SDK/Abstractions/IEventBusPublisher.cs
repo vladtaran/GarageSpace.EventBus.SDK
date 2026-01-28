97f00e13ccbe18e23af4edc2651b7f1dc6ef50f8
Taran.Soft <taran.soft@gmail.com>
2026-01-28 21:24:26 +0100

    initial commit

---
+++ src/GarageSpace.EventBus.SDK/Abstractions/IEventBusPublisher.cs
@@ -0,0 +1,24 @@
+﻿using GarageSpace.Contracts.Interfaces;
+
+namespace GarageSpace.EventBus.SDK.Abstractions
+{
+    public interface IEventBusPublisher
+    {
+        /// <summary>
+        /// Publishes a domain or integration event to the message bus.
+        /// </summary>
+        /// <typeparam name="TMessage">Type of the event.</typeparam>
+        /// <param name="event">Event payload.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken = default) where TMessage : IEventMessage;
+
+        /// <summary>
+        /// Publishes multiple events in a single operation.
+        /// Useful for outbox or batch publishing scenarios.
+        /// </summary>
+        /// <typeparam name="TMessage">Type of the events.</typeparam>
+        /// <param name="events">Collection of events.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        Task PublishBatchAsync<TMessage>(IReadOnlyCollection<TMessage> events, CancellationToken cancellationToken = default) where TMessage : IEventMessage;
+    }
+}
---
+++ src/GarageSpace.EventBus.SDK/EventBusPublisher.cs
@@ -0,0 +1,18 @@
+﻿using GarageSpace.Contracts.Interfaces;
+using GarageSpace.EventBus.SDK.Abstractions;
+
+namespace GarageSpace.EventBus.SDK
+{
+    internal class EventBusPublisher : IEventBusPublisher
+    {
+        public Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken = default) where TMessage : IEventMessage
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PublishBatchAsync<TMessage>(IReadOnlyCollection<TMessage> events, CancellationToken cancellationToken = default) where TMessage : IEventMessage
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
